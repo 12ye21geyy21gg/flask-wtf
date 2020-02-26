5c1f38f2c476ededc0c9e0c045631d757a62b85a
12ye21geyy21gg <12ye2112ye21>
2020-02-26 17:08:19 +0300

    3

---
+++ 3/serv.py
@@ -0,0 +1,17 @@
+from http.server import HTTPServer, CGIHTTPRequestHandler
+from flask import Flask, render_template,url_for
+
+app = Flask(__name__)
+
+
+@app.route('/list_prof')
+def list_prof():
+    param = {}
+    param['profs'] = list('инженер-исследователь, пилот, строитель, экзобиолог, врач, инженер по терраформированию, климатолог, специалист по радиационной защите, астрогеолог, гляциолог, инженер жизнеобеспечения, метеоролог, оператор марсохода, киберинженер, штурман, пилот дронов'.split(', '))
+    return render_template('base.html', **param)
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
+
+
+
+
