8ce76942b8eafb16c990c3debad59e8d30bc4a42
12ye21geyy21gg <12ye2112ye21>
2020-03-04 17:29:07 +0300

    sql

--- next_lesson/1/data/jobs.py
+++ next_lesson/1/data/jobs.py
@@ -1,20 +1,18 @@
-import datetime
+from sqlalchemy import orm
 import sqlalchemy
-from .db_session import SqlAlchemyBase
+import datetime
 
 
-# from .db_session import SqlAlchemyBase
-
-
-class User(SqlAlchemyBase):
-    __tablename__ = 'users'
-
+class Jobs():
+    __tablename__ = 'jobs'
     id = sqlalchemy.Column(sqlalchemy.Integer,
                            primary_key=True, autoincrement=True)
-    team_leader = sqlalchemy.Column(sqlalchemy.Integer)
+    team_leader = sqlalchemy.Column(sqlalchemy.Integer,
+                                    sqlalchemy.ForeignKey("users.id"))
     job = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     work_size = sqlalchemy.Column(sqlalchemy.Integer)
     collaborators = sqlalchemy.Column(sqlalchemy.String, nullable=True)
-    start_date = sqlalchemy.Column(sqlalchemy.DateTime)
+    start_date = sqlalchemy.Column(sqlalchemy.DateTime, default=datetime.datetime.now)
     end_date = sqlalchemy.Column(sqlalchemy.DateTime)
     is_finished = sqlalchemy.Column(sqlalchemy.Boolean)
+    job = orm.relation('users', back_populates='jobs')
